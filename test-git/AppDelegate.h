73139d7fece984929f764d5cb51157987e3e1ecd
frank <cyq8060@163.com>
2014-08-20 12:37:51 +0800

    test

---
+++ test-git/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  test-git
+//
+//  Created by Frank-Cai on 8/20/14.
+//  Copyright (c) 2014 GanJi. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ test-git/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  test-git
+//
+//  Created by Frank-Cai on 8/20/14.
+//  Copyright (c) 2014 GanJi. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
